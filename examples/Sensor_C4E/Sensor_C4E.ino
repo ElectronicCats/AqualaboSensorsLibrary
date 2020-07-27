e9653b84f6e138d58f31423a48615099b404e150
sabas1080 <s@electroniccats.com>
2020-07-26 22:13:46 -0500

    remove comments

--- examples/Sensor_C4E/Sensor_C4E.ino
+++ examples/Sensor_C4E/Sensor_C4E.ino
@@ -3,13 +3,6 @@
 aqualaboModbusSensorsClass aqualaboModbusSensors;
 
 void setup() {
-  /*
-  pinMode(MAX485_RE_NEG, OUTPUT);
-  pinMode(MAX485_DE, OUTPUT);
-  // Init in receive mode
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-  */
   Serial.begin(9600);
   while (!Serial);
   Serial.println("Modbus Sensor C4E");
