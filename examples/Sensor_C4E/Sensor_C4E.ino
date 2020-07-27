03eacf5a4c4a7db60fde6baa61ad339e4286ffec
sabas1080 <s@electroniccats.com>
2020-07-26 23:59:18 -0500

    Init example working in Serial2

--- examples/Sensor_C4E/Sensor_C4E.ino
+++ examples/Sensor_C4E/Sensor_C4E.ino
@@ -1,14 +1,35 @@
+/********************************************************************
+  Example using AqualaboSensors library to communicate
+  with C4E controller using a half-duplex RS485 transceiver.
+
+  This example is tested against an Sensor C4E.
+
+  Depended Library: ModbusFP
+
+  created 26 July 2020
+  by Author:: Andres Sabas @ Electronic Cats
+
+  This code is beerware; if you see me (or any other Electronic Cats
+  member) at the local, and you've found our code helpful,
+  please buy us a round!
+  Distributed as-is; no warranty is given.
+
+*********************************************************************/
 #include <AqualaboSensors.h>
 
 aqualaboModbusSensorsClass aqualaboModbusSensors;
 
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600);
+  
   while (!Serial);
   Serial.println("Modbus Sensor C4E");
   
-  aqualaboModbusSensors.begin(30, Serial2, 21, 30 );
+  aqualaboModbusSensors.begin(30, 30, 21, Serial2 );
+  Serial.print("Init sensor");
   aqualaboModbusSensors.initSensor();
+  Serial.print("Ready... ");
 }
 
 void loop() {
